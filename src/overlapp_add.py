e5e258b0b6eb3439876b0e528a7c2cbe3e69d771
Guglielmo Fratticioli <guglielmo.fratticioli@gmail.com>
2023-03-26 23:44:06 +0200

    working overlapp add

--- src/overlapp_add.py
+++ src/overlapp_add.py
@@ -1,32 +1,32 @@
 import numpy as np
-# how can i get the lenght of a numpy array
+import matplotlib.pyplot as plt
+
 def get_overlapped_frames(samples, overlap, window ): 
-    # Get array lenghts 
+# Get array lenghts 
     M = window.size
     N = len(samples)
     n_frames =int(np.ceil(N / (M - overlap)))
-    # Split frames into array 
+# Split frames into array 
     frames = [ np.zeros(M) for i in range(n_frames)]
     for i in range(n_frames):
         start = i*(M-overlap)
         end = np.clip(start + M ,0,N)
-        # Frame Windowing
+# Frame Windowing
         win_frame = np.zeros(M)
         win_frame[:(end-start)] += samples[start:end] * window[:(end-start)] 
-        # frames[ i*M : (i+1)*M] = win_frame
         frames[i] = win_frame
     return frames
     
-def overlapp_add_frames(frames, R, window):
-    M = window.size
-    N = len(frames)
-    overlap = (1-R)*M
-    out = np.zeros((M-overlap)*N)
+def overlapp_add_frames(frames, overlap, window=1):
+    if window is not 1 :  M = window.size
+    else : M = len(frames[0])
+    n_frames = len(frames)
+# initialize out buffer
+    out = np.zeros((M-overlap)*(n_frames))
     for i ,frame in enumerate(frames) : 
         start = i*(M - overlap)
-        end = start+M
-        out[start:end] += frame*window
-
+        end = np.clip(start+M,0,len(out))
+        out[start:end] += frame[:(end-start)]*window
     return out
         
 def check_COLA(window, R) :
@@ -36,12 +36,23 @@ def check_COLA(window, R) :
         for m in range(-window.size//R//2, window.size//R//2):
             sum += window[n+m*R]
         sums.append(sum)
-    # Check if sums are constant
+# Check if sums are constant
     if np.allclose(sums,sums[0]):
         return sums[0]
     else :
         return False 
 
 if __name__ == '__main__' : 
-    w = np.bartlett(100)
-    check_COLA(w,50)
+    N = 101
+    waveform = np.bartlett(N)
+    signal =  np.tile(waveform,10)
+    plt.figure()
+    plt.plot(signal)
+
+    window = np.bartlett(32)
+    print(check_COLA(window,1))
+    frames = get_overlapped_frames(signal,16,window)
+    signal2 = overlapp_add_frames(frames,16)
+    plt.figure()
+    plt.plot(signal2)
+    plt.show()
