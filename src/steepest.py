c82f32b29e257ffecc2cc2b06ec50b35cd6918f6
Guglielmo Fratticioli <guglielmo.fratticioli@gmail.com>
2023-03-28 13:01:44 +0200

    dsds

--- src/steepest.py
+++ src/steepest.py
@@ -2,6 +2,9 @@ import wave
 import numpy as np
 import matplotlib.pyplot as plt
 
+overlap_factor = 0.5
+frame_size = 1024
+window = np.hanning(frame_size)
 # Open the audio file named 'example.wav'
 
 def audio_read(file) : 
@@ -27,9 +30,44 @@ def divide_into_frames(signal, frame_size, overlap_factor):
         frames[i, :] = signal[i * step_size : i * step_size + frame_size]
     return frames
 
+def apply_window(frames, window_function):
+    return frames * window_function
+
+def compute_correlation(x,y): 
+    r = np.correlate(x,y,'full')
+    var = np.var(x)
+    N = len(x)
+    R = np.zeros((N, N))
+    for i in range(N):
+        R[i,:] = r[N-1-i:2*N-1-i]
+    R = R / var / N
+    return r, Rs
+
+def rec_find_weights(w,r,R,frame,mu,epsilon) : 
+    w_next = w + mu*(r + R*w)
+    J = np.mean(sum(frame^2)) - w*r - r*w + w*(R*w)
+    if J < epsilon: 
+        return w_next
+    else : 
+        return rec_find_weights(w_next,r,R,frame,mu,epsilon)
+
+def steepest(frames , init) : 
+    for frame in frames: 
+        w  = init
+        mu = 0.1
+        epsilon = 10^-3
+        r, R = compute_correlation(frame,frame)
+        w = rec_find_weights(w,r,R,frame,mu,epsilon)
+        plt.imshow(R, cmap='hot', interpolation='nearest')
+        plt.colorbar()
+        plt.show()
+
 if __name__ == '__main__':
 
     samples = audio_read('res/piano.wav')
-
-    plt.plot(samples)
+    frames = divide_into_frames(samples, frame_size, overlap_factor)
+    w_frames = apply_window(frames, window)
+    
+    predictors = steepest(w_frames)
+    plt.plot(w_frames[0])
     plt.show()
